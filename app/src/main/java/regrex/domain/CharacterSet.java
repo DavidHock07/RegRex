c98b61b3aba689f59f2ec18074a2ded85bd4a876
David Hock <davidhock45@gmail.com>
2024-02-25 22:00:09 +0100

    Added character sets

--- app/src/main/java/regrex/domain/CharacterSet.java
+++ app/src/main/java/regrex/domain/CharacterSet.java
@@ -1,5 +1,9 @@
 package domain;
 
 public class CharacterSet {
+    final static String upperCaseLetters = "ABCDEFGHUJKLMNOPQRSTUVWXYZ";
+    final static String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    final static String specialCharacters = "!\"#$%&()*+,-./:;<=>?@[]^_`{|}~";
+    final static String numbers = "0123456789";
 }
 
